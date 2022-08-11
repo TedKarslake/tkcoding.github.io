6b65f32723d87759769e4896b2755741738866af
Teddy K <89846460+TedKarslake@users.noreply.github.com>
2022-08-11 22:05:18 +0100

    Add files via upload

--- ipt_calc_v4.py
+++ ipt_calc_v4.py
@@ -1,6 +1,6 @@
-from def_round import custround
+from def_round import custround #custround will round the numbers to the second decimal place, for currency
 
-allowed_char1 = ['y', 'n'] #these are the allowed characters to begin
+allowed_char1 = ['y', 'n'] #this defines the acceptable inputs
 
 x = 1
 y = 1
@@ -20,31 +20,33 @@ while question == "y":
     print("")
     pcnt = input("Insurance Premium Tax Percent (number only): ")
     print("")
-    while x == 1:
+    while x == 1: #this is to prevent errors, so people can only input an int/float
         try:
             pcntnum = float(pcnt)
             x = 0
         except ValueError:
             pcnt = input("Error! Invalid character. Please enter an Insurance Premium Tax Percent number to continue: ")
             print("")
-
-    print("IPT: " + pcnt + "%")
-    print("")
     
+    pcnt_float = float(pcnt) #we had a problem where the input function above (to stop rogue inputs) would return a str not a float (messing up the sums below). This is to translate
+    print("Insurance Premium Tax: " + pcnt + "%")
+    print("")
+
     broker = input("Brokerage Percent (number only): ")
     print("")
-    while y == 1:
+    while y == 1: #as was used for Insurance Premium Tax above
         try:
             brokernum = float(broker)
             y = 0
         except ValueError:
-            broker = input("Error! Invalid character. Please enter a Brokerage Percent number to continue: ")
+            broker = float(input("Error! Invalid character. Please enter a Brokerage Percent number to continue: "))
             print("")
-    
-    allowed_char2 = ['p', 'g', 'n'] #as before
-    
+
+    broker_float = float(broker) #as above
     print("Brokerage: " + broker + "%")
     print("")
+
+    allowed_char2 = ['p', 'g', 'n'] #this defines the acceptable inputs
 
     while True:
         question2 = input("Press 'p' then [Enter key] for Net Premium input, 'g' then [Enter key] for Gross Premium input. Enter 'n' then [Enter key] at any time to stop: ")
@@ -56,34 +58,31 @@ while question == "y":
         else:
             break
 
-    if  question2 == "p":
-        pcnt_dec = float(pcnt / 100)
-        broker_dec = float(broker / 100)
+    if  question2 == "p": #'n' was already defined above to exit, so 'p' is for net 'p'remium
+        pcnt_dec = float(pcnt_float / 100)
+        broker_dec = float(broker_float / 100)
         print("")
-        net = input("Net Premium: £")
+        net = float(input("Net Premium: £"))
         try:
             netnum = float(net)
         except ValueError:
             print("")
             print("Error! Invalid character. Please enter a number to continue.")
             print("")
-        print("")
         sum = float(pcnt_dec * net)
         broker_sum = float(broker_dec * net)
         print("")
         print("Insurance Premium Tax Amount: £" + str(custround(sum)))
         print("")
         gross = net + sum
-        print(gross)
         print("Gross Premium: £" + str(custround(gross)))
         print("")
-        print(broker_sum)
         print("Brokerage due: £" + str(custround(broker_sum)))
         print("")
         print("Press 'n' at any time to stop")
 
-    elif question == "g":
-        broker_dec = float(broker / 100)
+    elif question2 == "g":
+        broker_dec = float(broker_float / 100)
         print("")
         gross = input("Gross Premium: £")
         try:
@@ -92,12 +91,15 @@ while question == "y":
             print("")
             print("Error! Invalid character. Please enter a number to continue.")
             print("")
-        pcnt_dec2 = float(pcnt / 100)
+
+        gross_float = float(gross) #as before to convert str to float
+
+        pcnt_dec2 = float(pcnt_float / 100)
         x = float(1 + pcnt_dec2)
-        sum2 = float(gross / x)
-        sum3 = float(sum2 - gross)
+        sum2 = float(gross_float / x)
+        sum3 = float(sum2 - gross_float)
         sum4 = float(sum3 * -1)
-        sum5 = float(gross - custround(sum4))
+        sum5 = float(gross_float - custround(sum4))
         broker_sum = float(broker_dec * sum5)
         print("")
         print("Insurance Premium Tax Amount: £" + str(custround(sum4)))
