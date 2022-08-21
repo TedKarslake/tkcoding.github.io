a275df27d7362fad286c093aa8e9b3d19fbcd28d
Teddy K <89846460+TedKarslake@users.noreply.github.com>
2022-08-21 21:09:51 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,10 @@
+let display = document.getElementById('display');
+
+let input = document.getElementByIput('ipt_amount');
+
+let input = document.getElementByIput('brokeage_amount');
+
+let input = document.getElementByIput('net_premium');
+
+let input = document.getElementByIput('gross_premium');
+
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-test3word = input("Tell me your lucky number: ")
-
-try:
-    test3num = int(test3word)
-    print("This is a valid number! Your lucky number is: ", test3num)
-except ValueError:
-    print("This is not a valid number. It isn't a number at all! This is a string, go and try again. Better luck next time!")
+import sys
+input=sys.argv[1]
+print(input)
